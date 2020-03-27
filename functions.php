e8567d60e8e5bc1cba2e2bc90fe3f4bb8d8bacc6
Graciela Alaniz <alanizcreative@gmail.com>
2020-03-27 03:35:01 -0400

    Mentor shortcode, archive fix mentors shortcode, layout, utility and page title updates

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@
  * -------------------
  */
 
+
 /* Add nav classes */
 
 function pma_nav_link_class( $atts, $item, $args ) {
@@ -28,6 +29,7 @@ function pma_nav_link_class( $atts, $item, $args ) {
 }
 add_filter( 'nav_menu_link_attributes' , 'pma_nav_link_class', 10, 3 );
 
+
 /* Enqueue styles and scripts */
 
 function pma_enqueue_styles() {
@@ -46,12 +48,14 @@ function pma_enqueue_styles() {
 }
 add_action( 'get_footer', 'pma_enqueue_styles' );
 
+
 /* Hide portfolio cpt */
 
 function pma_remove_menu_items() {
     remove_menu_page( 'edit.php?post_type=avada_portfolio' );
 }
 add_action( 'admin_menu', 'pma_remove_menu_items' );
+
 
 /* Load mentors on course backend */
 
@@ -77,13 +81,15 @@ function pma_acf_load_mentors( $field ) {
 }
 add_filter( 'acf/load_field/name=mentor', 'pma_acf_load_mentors' );
 
+
 /* Courses output shortcode */
 
 function pma_courses_shortcode( $atts ) {
 	$atts = shortcode_atts( [
 		'gradient' => false,
 		'category_slug' => '',
-        'posts_per_page' => 3
+        'posts_per_page' => 3,
+        'mentor_id' => 0
 	], $atts, 'courses' );
 
     extract( $atts );
@@ -105,10 +111,19 @@ function pma_courses_shortcode( $atts ) {
         ];
     }
 
+    if( $mentor_id ) {
+        $args['meta_query'] = [
+            [
+                'key' => 'mentor',
+                'value' => "$mentor_id",
+                'compare' => 'LIKE'
+            ]
+        ];
+    }
+
     // homepage meta
     if( is_front_page() ) {
         $args['meta_query'] = [
-            'relation' => 'AND',
             [
                 'key' => 'homepage',
                 'value' => '1',
@@ -137,7 +152,7 @@ function pma_courses_shortcode( $atts ) {
             ] );
 
             if( $featured_img ) {
-                $fig_classes = 'o-aspect-ratio --p-65 ' . ( $gradient ? 'o-gray__item' : 'u-opacity-70' );
+                $fig_classes = 'o-aspect-ratio --p-65 ' . ( $gradient ? 'o-gray__item' : 'u-op-70' );
                 $featured_img = "<a class='u-transform-scale" . ( $gradient ? '-fig' : '' ) . "' href='$url'><figure class='$fig_classes'>$featured_img</figure></a>";
             }
 
@@ -202,6 +217,7 @@ function pma_courses_shortcode( $atts ) {
 }
 add_shortcode( 'courses', 'pma_courses_shortcode' );
 
+
 /* Mentors output shortcode */
 
 function pma_mentors_shortcode( $atts ) {
@@ -213,12 +229,18 @@ function pma_mentors_shortcode( $atts ) {
 
     $output = '';
 
-    $args = [
-        'post_type' => 'mentor',
-        'posts_per_page' => $posts_per_page
-    ];
+    global $wp_query;
 
-    $q = new WP_Query( $args );
+    if( $wp_query->query_vars['post_type'] !== 'mentor' ) {
+        $args = [
+            'post_type' => 'mentor',
+            'posts_per_page' => $posts_per_page
+        ];
+
+        $q = new WP_Query( $args );
+    } else {
+        $q = $wp_query;
+    }
 
     if( $q->have_posts() ) {
         $output = '<div class="l-flex l-pad-h-lg l-pad-v-container u-fig --xl-b --wrap">';
@@ -236,10 +258,12 @@ function pma_mentors_shortcode( $atts ) {
             ] );
 
             if( $featured_img ) {
-                $fig_classes = 'l-w-150 o-aspect-ratio --circle';
+                $fig_classes = 'o-aspect-ratio --circle';
                 $featured_img =
                     "<div class='l-pad-h__item'>" .
-                        "<a class='u-transform-scale-fig' href='$url'><figure class='$fig_classes'>$featured_img</figure></a>" .
+                        "<div class='l-w-150'>" .
+                            "<a class='u-transform-scale-fig' href='$url'><figure class='$fig_classes'>$featured_img</figure></a>" .
+                        "</div>" .
                     "</div>";
             }
 
@@ -269,6 +293,7 @@ function pma_mentors_shortcode( $atts ) {
 }
 add_shortcode( 'mentors', 'pma_mentors_shortcode' );
 
+
 /* Testimonials output shortcode */
 
 function pma_testimonials_shortcode( $atts ) {
@@ -293,7 +318,7 @@ function pma_testimonials_shortcode( $atts ) {
     $q = new WP_Query( $args );
 
     if( $q->have_posts() ) {
-        $container_classes = 'l-pad-v-container --xxl-b';
+        $container_classes = 'u-fig l-pad-v-container --xxl-b';
 
         if( $horizontal )
             $container_classes .= ' l-pad-h l-flex l-flex-wrap-900';
@@ -313,10 +338,12 @@ function pma_testimonials_shortcode( $atts ) {
             ] );
 
             if( $featured_img ) {
-                $fig_classes = 'l-w-150 l-h-150 u-m-auto o-aspect-ratio --circle --p-0';
+                $fig_classes = 'o-aspect-ratio --circle';
                 $featured_img =
                     "<div class='l-pad-v-b" . ( !$horizontal ? ' l-pad-h__item ' : '' ) . "'>" .
-                        "<figure class='$fig_classes'>$featured_img</figure>" .
+                        "<div class='l-w-150 u-m-auto'>" .
+                            "<figure class='$fig_classes'>$featured_img</figure>" .
+                        "</div>" .
                     "</div>";
             }
 
@@ -360,3 +387,86 @@ function pma_testimonials_shortcode( $atts ) {
 	return $output;
 }
 add_shortcode( 'testimonials', 'pma_testimonials_shortcode' );
+
+
+/* Mentor output shortcode */
+
+function pma_mentor_shortcode( $atts ) {
+	$atts = shortcode_atts( [
+        'social' => true
+	], $atts, 'mentor' );
+
+    extract( $atts );
+
+    $social = $social === 'false' ? false : true;
+
+    $output = '';
+
+    $id = get_the_ID();
+    $name = get_the_title();
+    $artists = get_field( 'artists', $id );
+
+    $featured_img = get_the_post_thumbnail( $id, 'large', [
+        'class' => 'o-aspect-ratio__media u-object-cover'
+    ] );
+
+    if( $featured_img ) {
+        $fig_classes = 'o-aspect-ratio --circle';
+        $featured_img = "<figure class='$fig_classes'>$featured_img</figure>";
+    }
+
+    $social = '';
+        /*"<div class='l-pad-v-xl-t'>" .
+            "<div class='l-flex l-pad-h-sm --wrap --justify-center --align-center'>" .
+                "<div class='l-pad-h__item'>" .
+                    "<a class='o-social u-color-primary-light fusion-facebook fusion-icon-facebook' href='' rel='noopener noreferrer'>" .
+                        "<div class='u-visually-hidden'>Facebook</div>" .
+                    "</a>" .
+                "</div>" .
+                "<div class='l-pad-h__item'>" .
+                    "<a class='o-social u-color-primary-light fusion-twitter fusion-icon-twitter' href='' rel='noopener noreferrer'>" .
+                        "<div class='u-visually-hidden'>Twitter</div>" .
+                    "</a>" .
+                "</div>" .
+                "<div class='l-pad-h__item'>" .
+                    "<a class='o-social u-color-primary-light fusion-linkedin fusion-icon-linkedin' href='' rel='noopener noreferrer'>" .
+                        "<div class='u-visually-hidden'>Linkedin</div>" .
+                    "</a>" .
+                "</div>" .
+            "</div>" .
+        "</div>";*/
+
+    $left =
+        "<div class='l-w-300 l-pad-h__item l-pad-v-lg-b u-fig u-transform-y-900'>" .
+            $featured_img .
+            $social .
+        "</div>";
+
+    $content =
+        "<div class='l-pad-h__item'>" .
+            "<div class='l-pad-v-xl-b u-text-center-900'>" .
+                "<h1 class='u-m-0 l-pad-v-md-b'>$name</h1>" .
+                 ( $artists ? "<h3 class='u-m-0 u-fw-light'>$artists</h3>" : '' ) .
+            "</div>" .
+            do_shortcode( get_the_content() ) .
+        "</div>";
+
+    $output =
+        "<div class='l-flex l-flex-wrap-900 l-align-start l-pad-h-lg l-pad-v-xxxl-b'>" .
+            $left .
+            $content .
+        "</div>";
+
+    $courses = do_shortcode( '[courses mentor_id="' . $id . '" ]' );
+
+    if( $courses ) {
+        $output .=
+            "<div class='l-pad-v-xxxl-t l-pad-v-xxl-b u-b-top'>" .
+                "<h2 class='sm u-m-0 l-pad-v-xl-b u-text-align-center'>Courses</h2>" .
+                $courses .
+            "</div>";
+    }
+
+    return $output;
+}
+add_shortcode( 'mentor', 'pma_mentor_shortcode' );
